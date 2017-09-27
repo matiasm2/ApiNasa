ed1db10b731dd043c62e57604a4c0fc3f4431496
MatiasM <matiasmendoza1994@gmail.com>
2017-09-27 02:12:48 -0300

    Se agrega un generador de url basico tomando los parametros para q y media type.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-function mostrarmsj(){
-  $('<p>Hola grupoo</p>').appendTo('#resultados');
-  $('.texto').hide();
-  $('.formulario').hide();
-  $('#reintentar').show();
+function mostrarResultado(){
+  var url = "https://images-api.nasa.gov/search?q=";
+  generarUrl();
 
+
+}
+
+function generarUrl(){
+  var url = "https://images-api.nasa.gov/search?q=";
+  var texto = $('#texto').val();
+  if (texto){
+    url += texto+'&media_type='+$('#media_type').val();
+    console.log(url);
+    var p = '<p>'+url+'</p>';
+    $(p).appendTo('#resultados');
+    $('.texto').hide();
+    $('.formulario').hide();
+    $('#reintentar').show();
+
+  } else {
+    console.log('No ha ingresado el texto a buscar.');
+    $('#texto').focus();
+  }
 }
 
 function restaurar(){
