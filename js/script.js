f1b4118ec1f84be77604ce2860dc4a31a2ab1729
MatiasM <matiasmendoza1994@gmail.com>
2017-09-29 02:32:01 -0300

    Se modularizan algunas funciones y se agrega el manejo de etiquetas

--- js/script.js
+++ js/script.js
@@ -1,33 +1,63 @@
+/*
+Vista de contacto.
+Menu.
+Filtro fecha, APOD
+Agregar mas info a los recursos (autor y palabras claves)
+Enlazar palabras claves a nuevas consultas.
+Paginacion.
+Envio de correo.
+
+*/
+
 function mostrarResultado(){
-  generarUrl();
+  if(validarCampo()){
+    generarConsulta($('#texto').val());
+  }
+}
+
+function agregarImg(titulo, enlace){
+  var p = '<h3>Titulo: '+titulo+'</h3>';
+  p += '<img src='+enlace+' alt='+titulo+' width=100%>';
+  p += '<br><br>';
+  $(p).appendTo('#resultados');
+}
+
+function agregarVideo(titulo, nasa_id){
+  var p = '<h3>Titulo: '+titulo+'</h3>';
+  p +='<iframe id="'+nasa_id+'" type="text/html" width="100%"></iframe>';
+  p += '<br><br>';
+  $(p).appendTo('#resultados');
+  agregarUrlVideo(nasa_id);
+}
+
+function agregarEtiquetas(keywords){
+  keywords.forEach(function(item){
+    var func = "generarConsulta('";
+    func += item + "');";
+    var etiqueta = '<a href="#" onclick="'+func+'"">'+item+'</a>';
+    $(etiqueta).appendTo('#resultados');
+    console.log(item);
+  });
 }
 
 function ejecutarConsulta(url){
   $.ajax({
     url: url,
     success: function(result){
-      //console.log(result);
-      //console.log(JSON.stringify(result));
       result.collection.items.forEach(function(recurso){
-        //console.log(recurso);
         if (recurso.data[0].media_type=='image'){
-          var p = '<h3>Titulo: '+recurso.data[0].title+'</h3>';
-          p += '<img src='+recurso.links[0].href+' alt='+recurso.data[0].title+' width=100%>';
-          p += '<br><br>';
-          $(p).appendTo('#resultados');
+          agregarImg(recurso.data[0].title, recurso.links[0].href);
+          agregarEtiquetas(recurso.data[0].keywords);
         } if (recurso.data[0].media_type=='video'){
-          var p = '<h3>Titulo: '+recurso.data[0].title+'</h3>';
-          getUrlVideo(recurso.data[0].nasa_id);
-          p +='<iframe id="'+recurso.data[0].nasa_id+'" type="text/html" width="100%"></iframe>';
-          p += '<br><br>';
-          $(p).appendTo('#resultados');
+          agregarVideo(recurso.data[0].title, recurso.data[0].nasa_id);
+          agregarEtiquetas(recurso.data[0].keywords);
         }
       });
     }
   });
 }
 
-function getUrlVideo(nasa_id){
+function agregarUrlVideo(nasa_id){
   var url = 'https://images-api.nasa.gov/asset/'+nasa_id;
   var urlVideo = "";
   $.ajax({
@@ -37,25 +67,26 @@ function getUrlVideo(nasa_id){
       $('#'+nasa_id).attr('src',urlVideo);
     }
   });
-  return urlVideo;
 }
 
-
-function generarUrl(){
-  var url = "https://images-api.nasa.gov/search?q=";
-  var texto = $('#texto').val();
-  if (texto){
-    url += texto+'&media_type='+$('#media_type').val();
-    console.log(url);
-    ejecutarConsulta(url);
-    $('.texto').hide();
-    $('.formulario').hide();
-    $('#reintentar').show();
-
-  } else {
-    console.log('No ha ingresado el texto a buscar.');
-    $('#texto').focus();
+function validarCampo(){
+  if($('#texto').val()){
+    return true
   }
+  alert("No ha ingresado un parametro en el campo de texto.");
+  $('#texto').focus();
+  return false;
+}
+
+function generarConsulta(texto){
+  var url = "https://images-api.nasa.gov/search?q=";
+  url += texto+'&media_type='+$('#media_type').val();
+  console.log(url);
+  ejecutarConsulta(url);
+  $('.texto').hide();
+  $('#resultados').html('');
+  $('.formulario').hide();
+  $('#reintentar').show();
 }
 
 function restaurar(){
